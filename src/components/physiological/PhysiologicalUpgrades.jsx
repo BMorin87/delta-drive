c99687af9a8e8a377cd64b236c538fe2959182fb
Bradley Morin <bradleymorin@gmail.com>
2025-10-18 15:49:34 -0600

    Remove PhysiologicalUpgrades dependency on gameStore
    The upgradeStore subscribes to the gameStore to set/test state as needed.

--- src/components/physiological/PhysiologicalUpgrades.jsx
+++ src/components/physiological/PhysiologicalUpgrades.jsx
@@ -1,10 +1,7 @@
-import { useGameStore } from "../gameStore";
 import UpgradeItem from "../UpgradeItem";
 import "../../styles/physiological/PhysiologicalUpgrades.css";
 
 const PhysiologicalUpgradesPanel = () => {
-  const { volition } = useGameStore();
-
   return (
     <div className="physiological-upgrades-panel">
       <p className="upgrades-subtitle">Enhance your basic capabilities</p>
@@ -14,35 +11,30 @@ const PhysiologicalUpgradesPanel = () => {
           upgradeId="volitionRate"
           title="Determination"
           description="Increases volition generation rate"
-          currentVolition={volition}
         />
 
         <UpgradeItem
           upgradeId="volitionCapacity"
           title="Resilience"
           description="Increases volition capacity"
-          currentVolition={volition}
         />
 
         <UpgradeItem
           upgradeId="thirstReward"
           title="Hydration Reward"
           description="Increases volition gained from drinking"
-          currentVolition={volition}
         />
 
         <UpgradeItem
           upgradeId="hungerReward"
           title="Nourishment Reward"
           description="Increases volition gained from eating"
-          currentVolition={volition}
         />
 
         <UpgradeItem
           upgradeId="fatigueReward"
           title="Restoration Reward"
           description="Increases volition gained from resting"
-          currentVolition={volition}
         />
       </div>
     </div>
